aaf077bc5eae6b04d056ad581f948dace436731f
Lindy <rosone@126.com>
2018-11-14 20:41:43 +0800

    note change

--- app/src/main/java/com/gogovan/test/app/SampleApplication.java
+++ app/src/main/java/com/gogovan/test/app/SampleApplication.java
@@ -6,7 +6,7 @@ import com.gogovan.di.AppComponent;
 import com.gogovan.di.DaggerAppComponent;
 
 /**
- * application 使用dagger2 加载database的单例子
+ * application which use dagger2 to load database
  * Created by Arthur on 2018/11/8.
  */
 public class SampleApplication extends Application {
